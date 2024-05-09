3352bb8beed60dde8d7b0bead597ef6c090a95b6
Rahul BK <rlbishwokarma@gmail.com>
2024-05-09 16:09:30 +0545

    conversion route added

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
+const axios = require("axios");
 const config = require("./config/config");
 const app = express();
 
@@ -10,6 +11,34 @@ const apilimiter = rateLimit({
 // Middleware
 app.use(express.json()); // Pass incomming json data
 app.use(apilimiter);
+
+// Conversion routes
+app.post("/api/convert", async (req, res) => {
+  const { from, to, amount } = req.body;
+  try {
+    const url = `${config.exchangeRateApiUrl}/${
+      config.exchangeRateApiKey
+    }/pair/${from.toUpperCase()}/${to.toUpperCase()}/${amount}`;
+    const response = await axios.get(url);
+    if (response.data && response.data.result === "success")
+      res.json({
+        base: from,
+        target: to,
+        conversionRate: response.data.conversion_rate,
+        convertedAmount: response.data.conversion_result,
+      });
+    else
+      res.status(400).json({
+        message: "Error converting currency",
+        details: response.data,
+      });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error converting currency",
+      details: error.message,
+    });
+  }
+});
 
 // start the server
 app.listen(config.port, () => {
--- backend/config/config.js
+++ backend/config/config.js
@@ -3,6 +3,8 @@ dotenv.config();
 
 const _config = {
   port: process.env.PORT || 3005,
+  exchangeRateApiKey: process.env.EXCHANGE_RATE_API_KEY,
+  exchangeRateApiUrl: "https://v6.exchangerate-api.com/v6",
 };
 
 module.exports = Object.freeze(_config);
