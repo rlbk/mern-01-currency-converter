09996f84c39bce6bd6dcba13431104665889d56c
Rahul BK <rlbishwokarma@gmail.com>
2024-05-09 15:44:35 +0545

    middleware added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const config = require("./config/config");
 const app = express();
 
+const apilimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+});
 // Middleware
-app.use(express.json()); // parse incomming json data
+app.use(express.json()); // Pass incomming json data
+app.use(apilimiter);
 
 // start the server
 app.listen(config.port, () => {
